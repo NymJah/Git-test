b52491831dae0a17b285f9791c7224411bd9f94c
simon <simon@192.168.0.59>
2014-06-20 21:46:10 +0400

    i add !

--- Git-test-project/src/ua/ssi/test/Main.java
+++ Git-test-project/src/ua/ssi/test/Main.java
@@ -2,6 +2,6 @@ package ua.ssi.test;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("HEllo from Git Hub!!!");
+		System.out.println("HEllo from Git Hub!!!!");
 	}
 }
